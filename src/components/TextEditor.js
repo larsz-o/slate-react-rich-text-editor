c5759198960b9b3a4fc45f2435c1b5b6c18acd23
Lars <larsmackenzie@gmail.com>
2019-01-04 13:14:31 -0600

    bolding works with keyboard shortcuts and toggles

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
+import BoldMark from './BoldMark'; 
 
 // Create our initial value...
 const initialValue = Value.fromJSON({
@@ -35,9 +36,32 @@ class TextEditor extends Component {
     onChange = ({ value }) => {
       this.setState({ value })
     }
+    onKeyDown = (event, change) => {
+        // if the user is not pressing the control key, return out of the function
+        if(!event.ctrlKey){return}
+        event.preventDefault(); 
+        // if the user presses the control key, enter switch statement to determine what to do 
+        switch(event.key){
+            // when "b" is pressed, add a bold mark to the text
+            case 'b': {
+                change.toggleMark('bold')
+                return true; 
+            }
+        }
+    }
+
+    renderMark = props => {
+        switch(props.mark.type){
+            case 'bold': 
+            return <BoldMark {...props} />
+        }
+    }
     // Render the editor.
     render() {
-      return <Editor value={this.state.value} onChange={this.onChange} />
+      return <Editor value={this.state.value} 
+      onChange={this.onChange} 
+      onKeyDown={this.onKeyDown} 
+      renderMark={this.renderMark}/>
     }
   }
   export default TextEditor; 
